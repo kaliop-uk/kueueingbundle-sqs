29c2783c5662f87705e152f0998a1c78049560f4
gggeek <ggiunta@kaliop.com>
2019-09-29 17:28:38 +0000

    travis

--- Tests/app/config/config.php
+++ Tests/app/config/config.php
@@ -2,5 +2,5 @@
 // Used to set parameters based on env vars, regardless of the SF version in use
 
 // these environment variables are stored encrypted in travis.yml
-$container->setParameter('sqs_key', getenv('sqs.key'));
-$container->setParameter('sqs_secret', getenv('sqs.secret'));
+$container->setParameter('sqs_key', getenv('SYMFONY__SQS__KEY'));
+$container->setParameter('sqs_secret', getenv('SYMFONY__SQS__SECRET'));
